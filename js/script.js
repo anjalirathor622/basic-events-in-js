4add09433e622981e8ad4d48b433945e3e00ff47
anjalirathor622 <anjalirathor622@gmail.com>
2022-12-15 22:58:01 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -12,9 +12,33 @@ function hello(){
     }
 }
 
-// this function is for duoble click events 
+// this function is for double click events 
 function hello2(fisrtname,lastname) {
 
     document.getElementById("demo").innerHTML = 'hello ' + fisrtname + ' ' + lastname;
 }
 //hello2("anjali","rathor");
+
+//this one is for right click event
+function hello3(){
+    alert("hello,how are you?");
+}
+
+document.querySelector('p.ok').innerHTML= 'please right click here';
+
+
+// this function is for mouse hover event
+function hello4(){
+    alert(' you just moved your curser')
+}
+
+// this function is for mouse down event
+function hello5(){
+    alert("hey there")
+    //document.write('<h1> hey there</h1>')
+}
+
+// this function is for mouse up event
+function hello6(){
+    alert('ohhh you again!!!')
+}
